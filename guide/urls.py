bcee9e57017cb9f826d87bb8ef36ce771ec53a90
Arpita <arpitabhandage@gmail.com>
2026-04-11 13:39:59 +0530

    Fixed booking success

--- guide/urls.py
+++ guide/urls.py
@@ -46,11 +46,7 @@ urlpatterns = [
     #About us
     path('about/', views.about, name='about'),
 
-    #success
-    path('booking-success/', views.booking_success, name='booking_success'),
-
-    #contact
-    path('contact-success/', views.contact_success, name='contact_success')
+    
 
 ]
 
